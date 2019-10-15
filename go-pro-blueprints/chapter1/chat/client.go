edb942260b0195441e6a1266e9669720bb955b87
sergey.povarnin <sergey.povarnin@gmail.com>
2019-10-15 16:24:35 +0300

    GoProBlueprints: ch.1 - chat app. Step 4. Modeling room. Concurrency programming using. Turning a room into an HTTP Handler.

--- go-pro-blueprints/chapter1/chat/client.go
+++ go-pro-blueprints/chapter1/chat/client.go
@@ -34,9 +34,3 @@ func (c *client) write() {
 		}
 	}
 }
-
-type room struct {
-	// forward is a channel that holds incoming messages
-	// that should be forwarded to the other clients.
-	forward chan []byte
-}
