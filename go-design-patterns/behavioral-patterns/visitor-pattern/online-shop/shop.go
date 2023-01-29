1d10edb03fe97c8a0c85c9dba4504e82067fabec
LeeDark <sergey.povarnin@gmail.com>
2023-01-29 20:32:18 +0200

    Visitor: another example - online shop: added an idea with extended logic for Product inheritor - Fridge.

--- go-design-patterns/behavioral-patterns/visitor-pattern/online-shop/shop.go
+++ go-design-patterns/behavioral-patterns/visitor-pattern/online-shop/shop.go
@@ -7,3 +7,15 @@ type Rice struct {
 type Pasta struct {
 	Product
 }
+
+type Fridge struct {
+	Product
+}
+
+func (f *Fridge) GetPrice() float32 {
+	return f.Product.Price + 20
+}
+
+func (f *Fridge) Accept(v Visitor) {
+	v.Visit(f)
+}
