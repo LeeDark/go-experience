f19f67b97e05556a76f8ac44b04a8d7fa467f647
LeeDark <sergey.povarnin@gmail.com>
2023-01-29 19:09:58 +0200

    Added structs and interfaces for Visitor pattern.

---
+++ go-design-patterns/behavioral-patterns/visitor-pattern/visitor/visitor.go
@@ -0,0 +1,24 @@
+package visitor
+
+import "io"
+
+type MessageA struct {
+	Msg    string
+	Output io.Writer
+}
+
+type MessageB struct {
+	Msg    string
+	Output io.Writer
+}
+
+type Visitor interface {
+	VisitA(*MessageA)
+	VisitB(*MessageB)
+}
+
+type Visitable interface {
+	Accept(Visitor)
+}
+
+type MessageVisitor struct{}
